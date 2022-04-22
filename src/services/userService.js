0b1dbdb73e0bb6e494dfd4a5281ddbaf3d63a0be
VuThao <vuthaothao412@gmail.com>
2022-04-22 18:21:19 +0700

    update-thirty one

--- src/services/userService.js
+++ src/services/userService.js
@@ -49,6 +49,11 @@ const getDetailInforDoctor = (inputId) => {
 const saveBulkScheduleDoctor = (data) => {
   return axios.post("/api/bulk-create-schedule", data);
 };
+const getScheduleDoctorByDate = (doctorId, date) => {
+  return axios.get(
+    `/api/get-schedule-doctor-by-date?doctorId=${doctorId}&date=${date}`
+  );
+};
 
 export {
   handleLoginApi,
@@ -62,4 +67,5 @@ export {
   saveDetailDoctorService,
   getDetailInforDoctor,
   saveBulkScheduleDoctor,
+  getScheduleDoctorByDate,
 };
